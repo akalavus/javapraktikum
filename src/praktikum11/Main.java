b668c648148c3d6bc7c3656ee10feb255be97614
Andres Kalavus <akalavus@klass3-20-l.intra.itcollege.ee>
2016-11-02 11:28:51 +0200

    javakt

--- src/praktikum11/Main.java
+++ src/praktikum11/Main.java
@@ -5,6 +5,7 @@ import com.sun.javafx.geom.Rectangle;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,8 +14,8 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		Pane aken = new Pane();
 		
-		Rectangle kast = new Rectangle(120,60);//Siit jai pooleli!!!
-		
+		Rectangle kast = new Rectangle(100,100,300,200);//Siit jai pooleli!!!
+		aken.getChildren().add(kast);
 		
 		Scene stseen = new Scene(aken, 600, 400);
 		primaryStage.setScene(stseen);
